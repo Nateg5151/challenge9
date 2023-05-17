dcb9828e95a83fde6fd222aa37c1b443392b8cdd
Nathaniel Geurin <87617568+Nateg5151@users.noreply.github.com>
2023-05-17 18:52:08 -0500

    Add files via upload

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,112 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if (license === 'MIT') {
+    return `![License: MIT](https://img.shields.io/badge/License-MIT-blueviolet.svg)`
+  } else if (license === 'Apache 2.0') {
+    return `![License: Apache 2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`
+  } else if (license === 'None') {
+    return ''
+  }
+}
+
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return `https://opensource.org/licenses/MIT`
+  } else if (license === 'Apache 2.0') {
+    return `https://opensource.org/licenses/Apache-2.0`
+  } else if (license === 'None') {
+    return ''
+  }
+ }
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (license === 'MIT') {
+    return `MIT
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+      
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+      
+  THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  `
+  } else if (license === 'Apache 2.0') {
+    return `
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+ 
+      http://www.apache.org/licenses/LICENSE-2.0
+ 
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+    `
+  } else if (license === 'None') {
+    ''
+  }
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `
+  ${renderLicenseBadge(data.license)}
+
+# ${data.title} 
+
+## Description ℹ️
+
+${data.description}
+
+## Table of Contents 🔗
+
+✦[Installation](#Installation)
+
+✦[Usage](#Usage)
+
+✦[License](#License)
+
+✦[Contribute](#Contribute)
+
+✦[Tests](#Tests)
+
+✦[Contact Me](#Contact-me)
+
+## Installation ⚙️
+
+${data.installation}
+
+## Usage 🔧
+
+${data.usage}
+
+## License 🪪
+
+${renderLicenseLink(data.license)}
+
+${renderLicenseSection(data.license)}
+
+## How to Contribute 🏆
+
+${data.contributing}
+
+## Tests 🧩
+
+${data.tests}
+
+## Contact me ☎️
+
+[GitHub](${data.github})
+
+[Email](${data.email})
+`;
+}
+
+module.exports = generateMarkdown;
